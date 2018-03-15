8ad2446db0ac78f488cdb61b75040d18ecefe2cb
aidaBer <aidabernadettendiaye@gmail.com>
2018-03-15 14:03:05 +0000

    Modified commit

--- Model/model_arbitre.php
+++ Model/model_arbitre.php
@@ -5,14 +5,13 @@
 	return new PDO('mysql:host=localhost;dbname=bd;charset=utf8', 'root', 'root');
 }
 
-function ajouterArbitre($nom_arbitre,$prenom_arbitre,$ddn_arbitre,$poste_arbitre){
+function ajouterArbitre($nom_arbitre,$prenom_arbitre,$poste_arbitre){
 		$bdd1 = connection_arbitre();
 
-	    $result1=$bdd1->prepare("INSERT INTO `arbitre` (`nom_arbitre`, `prenom_arbitre`, `ddn_arbitre`, `id_equipe`) VALUES (:nom1, :prenom1, :ddn1,:poste1)");
-	    $result->execute(array(
+	    $result1=$bdd1->prepare("INSERT INTO `arbitre` (`nom_arbitre`, `prenom_arbitre`, `enum_poste`) VALUES (:nom1, :prenom1, :poste1)");
+	    $result1->execute(array(
 	    	'nom1'=>$nom_arbitre,
 	    	'prenom1'=>$prenom_arbitre,
-	    	'ddn1'=>$ddn_arbitre,
 	    	'poste1'=>$poste_arbitre ));
 }
 
