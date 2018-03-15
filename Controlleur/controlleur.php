918ffc31b1e3fe65cd8666c069bc22f5fac12431
Tamsir Ousmane Sall <tamsir-ousmane.sall@etu.u-pec.fr>
2018-03-15 04:20:29 +0100

    Test insertion joueur

--- Controlleur/controlleur.php
+++ Controlleur/controlleur.php
@@ -1,3 +1,8 @@
 <?php
+	include '../Model/model.php';
+
+	if(isset($_REQUEST["joueur"])){
+		ajouterJoueur($_REQUEST["nom"],$_REQUEST["prenom"],$_REQUEST["nationalite"],67);
+	}
 
 ?>
--- Model/model.php
+++ Model/model.php
@@ -1,2 +1,22 @@
 <?php
+
+	function connection()
+{
+	return new PDO('mysql:host=localhost;dbname=bd;charset=utf8', 'root', 'root');
+}
+
+
+	function ajouterJoueur($nom,$prenom,$nationalite,$equipe){
+		$bdd = connection();
+
+	    $result=$bdd->prepare("INSERT INTO `joueur` (`nom_joueur`, `prenom_joueur`, `nationalite_joueur`, `id_equipe`) VALUES (:nom, :prenom, :nationalite,:equipe)");
+	    $result->execute(array(
+	    	'nom'=>$nom,
+	    	'prenom'=>$prenom,
+	    	'nationalite'=>$nationalite,
+	    	'equipe'=>$equipe
+	));
+	}
+
+
 ?>
